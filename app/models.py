2d142aef7acc3ed04bcdfc9d5cf9501b083e5f11
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-12-04 15:43:24 +0500

    Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+    category = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.name
